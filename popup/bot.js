040bf090899409e8a3c1b033a677dbd879b9fe04
Caroline Chong <35038335+exokaroline@users.noreply.github.com>
2018-09-23 14:15:32 -0400

    popup

---
+++ popup/bot.js
@@ -0,0 +1,16 @@
+var account = "https://api.twitter.com/1.1/account/update_profile_image.json?image=ABCDEFGH…"
+// auth = //key taken out'
+function getUserData() 
+{
+	profile_pic = new twitter.account(document.getElementById('profile_image_url'),
+	username = new twitter.account(document.getElementById('screen_name')),
+	name = new twitter.account(document.getElementById('name'));
+}
+
+
+function getProfilePic( ) {
+  if(location.hostname.match('twitter.com')){
+  	
+
+  }
+}
